64fe1360c362d53b768ad1fed90044c004398d27
jpaola09 <jpninom@itc.edu.co>
2021-10-24 19:59:12 -0500

    make it better

--- build/config/mongoose.js
+++ build/config/mongoose.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 //@ts-ignore
 const mongoose_1 = __importDefault(require("mongoose"));
-const MONGO_URI = 'mongodb+srv://root:1994@alquiler.vjmvn.mongodb.net/Alquiler?retryWrites=true&w=majority';
+const MONGO_URI = 'mongodb+srv://root:1994@rental.vjmvn.mongodb.net/rental?retryWrites=true&w=majority';
 mongoose_1.default.set('useFindAndModify', false);
 mongoose_1.default.connect(MONGO_URI || process.env.MONGODB_URL, {
     useNewUrlParser: true,
--- src/config/mongoose.ts
+++ src/config/mongoose.ts
@@ -1,6 +1,6 @@
 //@ts-ignore
 import mongoose from 'mongoose';
-const MONGO_URI = 'mongodb+srv://root:1994@alquiler.vjmvn.mongodb.net/Alquiler?retryWrites=true&w=majority';
+const MONGO_URI = 'mongodb+srv://root:1994@rental.vjmvn.mongodb.net/rental?retryWrites=true&w=majority';
 mongoose.set('useFindAndModify', false);
 mongoose.connect(MONGO_URI || process.env.MONGODB_URL,{
     useNewUrlParser: true,
